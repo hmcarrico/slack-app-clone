14053bcb770ed8f3786055751bd260aefea08c47
Hunter Carrico <huntercarrico@Hunters-Air.frontierlocal.net>
2018-12-28 13:07:37 -0800

    sidebar progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import {Grid} from 'semantic-ui-react';
 import './App.css';
+import {connect} from 'react-redux';
 
-const App = () => (
-  <Grid>
-    <ColorPanel />
-    <SidePanel />
-    <Messages />
-    <MataPanel />
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanels/MetaPanels';
+
+const App = ({currentUser}) => (
+  <Grid columns='equal' className='app' style={{background: '#eee'}}>
+    <ColorPanel/>
+    <SidePanel currentUser={currentUser}/>
+
+    <Grid.Column style={{marginLeft: 320}}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
   </Grid>
 )
 
-export default App;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/MetaPanels/MetaPanels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MetaPanels extends React.Component {
+    render(){
+        return (
+            <div>MetaPanels</div>
+        )
+    }
+}
+
+export default MetaPanels;
